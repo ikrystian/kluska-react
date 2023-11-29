0e021ae17003d1a3154c703af5a06595226562c6
Krystian <krystian.kuzminski@programa.pl>
2023-11-29 23:35:49 +0100

    Add new inputs into login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { TextField } from '@mui/material';
 import axios from 'axios';
-import { signal } from "@preact/signals-core";
 import { token } from '../router/auth/AuthSignal.ts';
+
+class FromData {
+}
 
 export default function LoginForm() {
 
-    const userForm = signal({email: '', password: ''})
+    type FormData = {
+        email: string;
+        password: string;
+    }
 
+    const schema = yup
+        .object()
+        .shape(
+            {
+                email: yup.string().email().required(),
+                password: yup.string().required(),
+            }
+        )
+        .required()
 
-    const login = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        const user = {
-            email: userForm.value.email,
-            password: userForm.value.password
-        }
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>({
+        resolver: yupResolver(schema)
+    })
 
-        axios.post(`${import.meta.env.VITE_API_URL}auth/signin`, user, {
+    const onSubmit = (data: FromData) => {
+        axios.post(`${import.meta.env.VITE_API_URL}auth/signin`, data, {
             headers: {'Content-Type': 'application/json'}
         })
             .then(res => {
@@ -28,24 +43,18 @@ export default function LoginForm() {
                 console.error(error);
             })
     }
+
     return (
-        <form onSubmit={login}>
-            <div className={'flex flex-col gap-2 mb-2'}>
-                <label htmlFor="email"
-                       className={'text-white'} >E-mail</label>
-                <input type="email" id="email" name="email"
-                       onChange={(event) => {
-                           userForm.value.email = event.target.value;
-                       }}
-                       className="email py-1 px-3 outline" required/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={'flex flex-col gap-2 mb-5'}>
+                <TextField type="email" {...register('email')} error={Boolean(errors.email)}
+                           helperText={errors.email && errors.email.message} id="email" label="E-mail"
+                           variant="outlined"/>
             </div>
-            <div className={'flex flex-col mb-5'}>
-                <label htmlFor="password" className={'text-white'}>Password</label>
-                <input type="password" id="password" name="password" className="password  py-1 px-3 outline"
-                       onChange={(event) => {
-                           userForm.value.password = event.target.value;
-                       }}
-                       required/>
+            <div className={'flex flex-col gap-2 mb-2'}>
+                <TextField type="password" {...register('password')} error={Boolean(errors.password)}
+                           helperText={errors.password && errors.password.message} id="password" label="Password"
+                           variant="outlined"/>
             </div>
             <button type="submit" className={'block w-full p-2 bg-blue-950 mb-5 text-white'}>Log in</button>
         </form>
