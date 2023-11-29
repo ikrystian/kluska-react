d62e90a2c443c2766775634753c6a3b1a8e178a7
Krystian <krystian.kuzminski@programa.pl>
2023-11-29 18:09:59 +0100

    Add avatar in user dashboard

--- src/router/home/Dashboard.tsx
+++ src/router/home/Dashboard.tsx
@@ -1,34 +1,80 @@
-import DashboardNavComponent from '../../components/DashboardNavComponent.tsx';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
-interface User {
-    id: string;
-    name: string;
-    email: string;
+import { useState } from 'react';
+import { AppBar, Box, Tab, Tabs, Typography } from '@mui/material';
+import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
+import SwipeableViews from 'react-swipeable-views';
+
+
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    dir?: string;
+    index: number;
+    value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const {children, value, index, ...other} = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{p: 3}}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
 }
 
 export default function Dashboard() {
-    const [user, setUser] = useState<User | null>(null);
 
-    useEffect(() => {
-        const config = {
-            headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
-        };
-        axios.get(`${import.meta.env.VITE_API_URL}auth/user`, config).then(res => {
-            setUser(res.data)
-            console.log(res.data);
-        })
-    }, []);
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        console.log(event);
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = (index: number) => {
+        setValue(index);
+    };
 
     return (
-        <main className={'h-[100dvh] flex flex-col'}>
-            <div className={'px-3 flex-1'}>
-                <h2 className={'text-white'}>Witaj <strong>{user && user.name}</strong></h2>
-            </div>
-            <div>
-                <DashboardNavComponent/>
-            </div>
+        <main className={'flex flex-auto flex-col justify-end'}>
+            <SwipeableViews
+                index={value}
+                className={'h-full text-white'}
+                onChangeIndex={handleChangeIndex}
+            >
+                <TabPanel value={value} index={0}>
+                   Add training
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    Stats
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    History
+                </TabPanel>
+            </SwipeableViews>
+            <AppBar position="static">
+                <Tabs value={value} onChange={handleChange}
+                      indicatorColor="secondary"
+                      textColor="inherit"
+                      variant="fullWidth">a
+                    <Tab icon={<SpaceDashboardIcon/>} aria-label="phone"/>
+                    <Tab icon={<FavoriteIcon/>} aria-label="favorite"/>
+                    <Tab icon={<PersonPinIcon/>} aria-label="person"/>
+                </Tabs>
+            </AppBar>
         </main>
     )
 }
--- src/router/home/Home.tsx
+++ src/router/home/Home.tsx
@@ -1,10 +1,50 @@
+import IconButton from "@mui/material/IconButton";
 import { Outlet } from "react-router-dom";
+import HomeIcon from '@mui/icons-material/Home';
+import Avatar from "@mui/material/Avatar";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
 export default function Home() {
+
+    interface User {
+        id: string;
+        name: string;
+        email: string;
+    }
+
+    function stringAvatar(name: string) {
+        if(!name) return;
+        return {
+            sx: {
+                bgcolor: '#090909',
+            },
+            children: `${name[0]}`,
+        };
+    }
+
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const config = {
+            headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+        };
+        axios.get(`${import.meta.env.VITE_API_URL}auth/user`, config).then(res => {
+            setUser(res.data)
+        })
+    }, []);
+
     return (
         <>
-            {/* all the other elements */}
-            <div id="home" className="h-[100dvh] bg-[#1A1B1F]">
-                <Outlet />
+            <div id="home"  className="h-[100dvh] bg-[#1A1B1F] flex flex-col ">
+                <Outlet/>
+                <nav className={'flex items-center gap-2 py-1 px-2'}>
+                    {user && <Avatar  {...stringAvatar(user.name)} /> }
+                    <IconButton aria-label="delete" size="large" color="primary">
+                        <HomeIcon/>
+                    </IconButton>
+                </nav>
             </div>
         </>
     );
