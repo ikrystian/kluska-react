cb4e18c517051b35d338eff33fd67c7ed27a5beb
Krystian <krystian.kuzminski@programa.pl>
2023-11-29 08:03:02 +0100

    Change state to signal

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
+import { signal } from "@preact/signals-core";
 
 export default function LoginForm() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
+    const email = signal('krystian@bpcoders.pl');
+    const password = signal('');
 
     const login = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         const user = {
-            email,
-            password
+            email: email.value,
+            password: password.value
         }
 
-        axios.post(`${import.meta.env.VITE_API_KEY}auth/signin`, user, {
+        axios.post(`${import.meta.env.VITE_API_URL}auth/signin`, user, {
             headers: {'Content-Type': 'application/json'}
         })
             .then(res => {
@@ -22,10 +22,13 @@ export default function LoginForm() {
                     localStorage.setItem('token', token);
                     alert(`Loggedn, your token is ${token}`)
                 }
+
             })
             .catch((error) => {
                 if (error.response.status === 401) {
                     alert('Unauthorized');
+                } else if (error.response.status === 422) {
+                    alert(error.response.data.password[0])
                 } else {
                     console.error(error)
                 }
@@ -35,13 +38,19 @@ export default function LoginForm() {
         <form onSubmit={login}>
             <div className={'flex flex-col mb-2'}>
                 <label htmlFor="email" className={'text-white mb-1'}>E-mail</label>
-                <input type="email" id="email" name="email" value={email} className="email py-1 px-3 outline"
-                       onChange={(event) => setEmail(event.target.value)} required/>
+                <input type="email" id="email" name="email"
+                       onChange={(event) => {
+                           email.value = event.target.value;
+                       }}
+                       className="email py-1 px-3 outline" required/>
             </div>
             <div className={'flex flex-col mb-5'}>
                 <label htmlFor="password" className={'text-white mb-1'}>Password</label>
-                <input type="passowrd" id="password" name="password" value={password} className="password  py-1 px-3 outline"
-                       onChange={(event) => setPassword(event.target.value)} required/>
+                <input type="password" id="password" name="password" className="password  py-1 px-3 outline"
+                       onChange={(event) => {
+                           password.value = event.target.value;
+                       }}
+                       required/>
             </div>
             <button type="submit" className={'block w-full p-2 bg-blue-950 mb-5 text-white'}>Log in</button>
         </form>
