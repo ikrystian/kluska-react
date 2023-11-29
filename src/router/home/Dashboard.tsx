22ed491042efb7578cbaae2a257eff7a190abc19
Krystian <krystian.kuzminski@programa.pl>
2023-11-29 21:31:40 +0100

    Fix for summary

--- src/router/home/Dashboard.tsx
+++ src/router/home/Dashboard.tsx
@@ -1,37 +1,36 @@
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import FolderIcon from '@mui/icons-material/Folder';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 
 export default function Dashboard() {
-
-
     return (
         <main className={'flex flex-auto flex-col justify-end'}>
             <Outlet/>
-            <BottomNavigation sx={{ width: 500 }}  value={useLocation().pathname}>
+
+            <BottomNavigation  value={useLocation().pathname}>
                 <BottomNavigationAction
                     label="Recents"
                     icon={<RestoreIcon />}
                     component={Link}
                     to="/home/dashboard/"
                 />
+
                 <BottomNavigationAction
                     label="Stats"
                     icon={<FavoriteIcon />}
                     component={Link}
                     to="/home/dashboard/summary"
                 />
+
                 <BottomNavigationAction
                     label="Archive"
                     icon={<LocationOnIcon />}
                     component={Link}
                     to="/home/dashboard/archive"
                 />
-                <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
             </BottomNavigation>
         </main>
     )
--- src/router/home/Summary.tsx
+++ src/router/home/Summary.tsx
@@ -1,18 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getRequest } from '../../helpers.ts';
 
 export default function Summary() {
-
+    const [stats, setStats] = useState(null);
 
     useEffect(() => {
         getRequest('stats').then(res => {
-            console.log(res)
+            setStats(res);
         })
     }, []);
 
+
+
     return (
-        <section className={'flex flex-1 py-4 px-2'}>
-            Summary
+        <section className={'flex flex-1 py-4 px-2 text-white'}>
+            <div>
+                Summary
+                <p>Trainings: {stats && stats.trainings}</p>
+                <p>RUN: {stats && stats.runMeters}m</p>
+                <p>TONS: {stats && stats.total}kg</p>
+            </div>
         </section>
     )
 }
